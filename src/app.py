9a28d08d462a7fe43c6cdd80f8f824d48c4198fa
Guilherme Frainer <guilherme.sfrainer@gmail.com>
2024-12-09 18:46:15 -0300

    Allows user to pick number of rows to be displayed

--- src/app.py
+++ src/app.py
@@ -6,6 +6,8 @@ import treemap as tm
 
 
 DATA_DIR = "data/"
+MIN_ROW = 0
+MAX_ROW = 35
 DATA_FILES = {
     "BookCrossing": "book_crossing.csv",
     "Yelp": "yelp.csv",
@@ -23,11 +25,14 @@ def main():
 
     dataset = st.sidebar.selectbox("Dataset", dataset_options)
     algorithm = ALGORITHMS[st.sidebar.selectbox("Algorithm", algo_options)]
+    rows_to_display = st.sidebar.number_input(
+        "Rows to display", min_value=MIN_ROW + 1, max_value=MAX_ROW)
 
     df = pl.read_csv(DATA_DIR + DATA_FILES[dataset])
-    filtered_df = df.filter(pl.col("algorithm") == algorithm)
+    filtered_df = df.filter(pl.col("algorithm") == algorithm) \
+        .filter(pl.col("step") < rows_to_display)
     hs = hr.HypothesisRow.df_to_list(filtered_df)
-    treemap = tm.make_treemap_from_range(hs, 0, 20)
+    treemap = tm.make_treemap_from_range(hs, MIN_ROW, rows_to_display)
 
     option = st.sidebar.selectbox("How do you want to visualize it?", ["Treemap", "Data frame"])
     if option == "Data frame":
