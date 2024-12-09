04c880b4cc1e5d894672b002bc2f83f0144e2d1c
Guilherme Frainer <guilherme.sfrainer@gmail.com>
2024-12-09 18:39:56 -0300

    Allows user to filter based on algorithm

--- src/app.py
+++ src/app.py
@@ -11,19 +11,27 @@ DATA_FILES = {
     "Yelp": "yelp.csv",
     "MovieLens": "movie_lens.csv",
 }
+ALGORITHMS = {
+    "Greedy": "Greedy-HEP",
+    "Reinforcement Learning": "RL-HEP",
+}
 
 
 def main():
-    dataset = st.sidebar.selectbox("Dataset", [k for k in DATA_FILES])
+    dataset_options = [k for k in DATA_FILES]
+    algo_options = [k for k in ALGORITHMS]
+
+    dataset = st.sidebar.selectbox("Dataset", dataset_options)
+    algorithm = ALGORITHMS[st.sidebar.selectbox("Algorithm", algo_options)]
 
     df = pl.read_csv(DATA_DIR + DATA_FILES[dataset])
-    hs = hr.HypothesisRow.df_to_list(df)
+    filtered_df = df.filter(pl.col("algorithm") == algorithm)
+    hs = hr.HypothesisRow.df_to_list(filtered_df)
     treemap = tm.make_treemap_from_range(hs, 0, 20)
 
-    # TODO: The prompt here could be better
     option = st.sidebar.selectbox("How do you want to visualize it?", ["Treemap", "Data frame"])
     if option == "Data frame":
-        df
+        filtered_df
     elif option == "Treemap":
         treemap
 
